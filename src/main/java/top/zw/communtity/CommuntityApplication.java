9c9cf29228358a6fc9dde10eefc1dc54279d5f09
wangzaiaaa <2627894288@qq.com>
2019-11-22 14:01:35 +0800

    first commit

---
+++ src/main/java/top/zw/communtity/CommuntityApplication.java
@@ -0,0 +1,14 @@
+package top.zw.communtity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class CommuntityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommuntityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/zw/communtity/CommuntityApplicationTests.java
@@ -0,0 +1,13 @@
+package top.zw.communtity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommuntityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
